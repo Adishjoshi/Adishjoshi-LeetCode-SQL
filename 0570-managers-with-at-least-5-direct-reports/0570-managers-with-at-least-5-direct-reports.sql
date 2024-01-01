f93782ad53ba694f383bae3df76acc39ba6ffe7d
Adishjoshi <67081361+Adishjoshi@users.noreply.github.com>
2024-01-01 18:21:43 -0500

    Time: 642 ms (38.01%), Space: 0B (100.00%) - LeetHub

---
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -0,0 +1,6 @@
+Select e1.name
+from employee as e
+inner join employee as e1
+on e.managerId= e1.id #join on manager id from e table on the emp_id from the e1 table
+group by e.managerId 
+having count(e.id)>=5
