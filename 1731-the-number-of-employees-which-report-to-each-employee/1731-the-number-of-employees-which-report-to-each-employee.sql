6bb9302da7c0d6c836fc46a4c6cbc1568a9d45c8
Adishjoshi <67081361+Adishjoshi@users.noreply.github.com>
2024-01-30 19:24:52 -0500

    Time: 1233 ms (40.72%), Space: 0B (100.00%) - LeetHub

--- 1731-the-number-of-employees-which-report-to-each-employee/1731-the-number-of-employees-which-report-to-each-employee.sql
+++ 1731-the-number-of-employees-which-report-to-each-employee/1731-the-number-of-employees-which-report-to-each-employee.sql
@@ -1,8 +1,10 @@
+
+
 select 
-        e1.employee_id, 
-        e1.name, 
-        count(e2.reports_to) as reports_count, 
-        round(avg(e2.age)) as average_age 
+e1.employee_id, 
+e1.name, 
+count(e2.reports_to) as reports_count, 
+round(avg(e2.age)) as average_age 
 from Employees e1 
 left join Employees e2 on e1.employee_id=e2.reports_to 
 where e2.reports_to is not null
