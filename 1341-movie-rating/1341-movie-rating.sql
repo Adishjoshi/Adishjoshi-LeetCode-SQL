23488be649de43e6457e0116092658103a5e5203
Adishjoshi <67081361+Adishjoshi@users.noreply.github.com>
2024-09-29 15:12:13 -0400

    Time: 1303 ms (30.77%), Space: 0B (100%) - LeetHub

---
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+with cte as 
+    (select u.name as results from users u join movierating m on u.user_id = m.user_id
+    group by u.name
+    order by count(m.movie_id) desc, u.name asc
+    limit 1),
+cte2 as
+    (select m.title as results from movies m join movierating mr on m.movie_id = mr.movie_id
+    where mr.created_at >= "2020-02-01" and mr.created_at <= "2020-02-29"
+    group by m.title
+    order by avg(mr.rating) desc, m.title asc
+    limit 1
+    )
+select results from cte
+union all
+select results from cte2
