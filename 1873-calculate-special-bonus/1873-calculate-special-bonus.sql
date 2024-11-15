f31c599b9bcc205d2501cfaf133a16acc55cc40b
Adishjoshi <67081361+Adishjoshi@users.noreply.github.com>
2024-11-15 14:35:15 -0500

    Time: 875 ms (13.7%), Space: 0B (100%) - LeetHub

--- 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
+++ 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
@@ -1,5 +1,7 @@
-# Write your MySQL query statement below
-select employee_id,
-if(employee_id%2=1 and name NOT REGEXP '^M', salary, 0) as bonus 
-from employees 
-order by employee_id;
+   select employee_id , 
+   case when employee_id%2 <>0 and name not like 'M%' then salary 
+   else 0
+   end 
+   as bonus
+   from employees
+   order by employee_id
