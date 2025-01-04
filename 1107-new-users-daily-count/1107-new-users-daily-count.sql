70c93408367db533b356ce14a10f96bce5bcfd89
Adishjoshi <67081361+Adishjoshi@users.noreply.github.com>
2025-01-04 15:54:58 -0500

    Time: 433 ms (96.59%), Space: 0B (100%) - LeetHub

--- 1107-new-users-daily-count/1107-new-users-daily-count.sql
+++ 1107-new-users-daily-count/1107-new-users-daily-count.sql
@@ -1,22 +1,19 @@
-SELECT 
-  activity_date AS login_date, 
-  COUNT(DISTINCT user_id) AS user_count 
-FROM 
-  (
+# Write your MySQL query statement below
+
+WITH cte AS (
     SELECT 
-      *, 
-      RANK() OVER (
-        PARTITION BY user_id 
-        ORDER BY 
-          activity_date ASC
-      ) AS rnk 
-    FROM 
-      Traffic 
+        activity_date,
+        user_id,
+        RANK() OVER (PARTITION BY user_id ORDER BY activity_date ASC) AS rnk
+    FROM Traffic
     WHERE 
-      activity = 'login'
-  ) t0 
-WHERE 
-  rnk = 1 
-  AND DATEDIFF('2019-06-30', activity_date) <= 90 
-GROUP BY 
-  activity_date
+        
+        activity = 'login'
+)
+SELECT 
+    activity_date AS login_date,
+    COUNT( distinct user_id) AS user_count
+FROM cte
+WHERE rnk = 1 and activity_date BETWEEN DATE_SUB('2019-06-30', INTERVAL 90 DAY) AND '2019-06-30'
+GROUP BY activity_date;
+
