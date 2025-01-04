bceac2fc779c8de90dad56fb139bf4677280528c
Adishjoshi <67081361+Adishjoshi@users.noreply.github.com>
2025-01-03 21:34:23 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
+++ 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
@@ -1,14 +1,9 @@
 # Write your MySQL query statement below
-SELECT
-  customer_id
-FROM
-  Customer
-GROUP BY
-  customer_id
-HAVING
-  COUNT(DISTINCT product_key) = (
-    SELECT
-      COUNT(product_key)
-    FROM
-      Product
-  );
+with cte as (select 
+*, row_number() over(partition by customer_id order by customer_id asc) as num
+from 
+Customer
+order by customer_id )
+
+select customer_id from cte 
+where num >= 2 
