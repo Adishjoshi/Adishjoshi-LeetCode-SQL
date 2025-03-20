257af650a27b0e1d6d661ea2c4598934bdcdeb6f
Adishjoshi <67081361+Adishjoshi@users.noreply.github.com>
2025-03-20 17:41:29 -0400

    Time: 308 ms (95.93%), Space: 0B (100%) - LeetHub

--- 0178-rank-scores/0178-rank-scores.sql
+++ 0178-rank-scores/0178-rank-scores.sql
@@ -1,2 +1,4 @@
-select score, dense_rank() over (order by score desc) as 'rank' #add ' ' single quotes over rank as it is a sql function 
+select 
+score
+,dense_rank() over(order by score desc) as "rank"
 from Scores
