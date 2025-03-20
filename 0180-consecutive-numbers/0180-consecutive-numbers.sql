7a3f51c9a985e26f31fdeee4aad2f06d535018e1
Adishjoshi <67081361+Adishjoshi@users.noreply.github.com>
2025-03-20 17:51:44 -0400

    Time: 1925 ms (95.09%), Space: 0B (100%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -1,12 +1,8 @@
-WITH cte AS (
-    SELECT 
-        id,
-        num,
-        LEAD(num, 1) OVER (ORDER BY id ASC) AS lead_num1,
-        LEAD(num, 2) OVER (ORDER BY id ASC) AS lead_num2
-    FROM Logs
-)
+with cte as (select *
+, lead(num,1) over() as next1
+, lead(num,2) over() as next2
+from logs)
 
-SELECT distinct num AS ConsecutiveNums
-FROM cte
-WHERE num = lead_num1 AND num = lead_num2
+select distinct num as ConsecutiveNums
+from cte 
+where num = next1 and num = next2
