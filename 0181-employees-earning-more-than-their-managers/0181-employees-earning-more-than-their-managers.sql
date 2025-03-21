490ba3c665a195cf765fb38be336283069957151
Adishjoshi <67081361+Adishjoshi@users.noreply.github.com>
2025-03-20 22:46:36 -0400

    Time: 386 ms (95.15%), Space: 0B (100%) - LeetHub

--- 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
@@ -1,15 +1,12 @@
-# Write your MySQL query statement below
-with cte as (select 
-e.id
-,e.name as Employee_name
-,e.salary as Emp_salary
+with cte as(
+select 
+e.id as emp_id
+,e.name as "Employee"
+,e.salary as "emp_salary"
 
-,e1.name as Mamager_name
-,e1.salary as Mgr_Salary
+,m.salary
+from Employee as e left join Employee as m on e.managerId = m.id
+having e.salary > m.salary
+)
 
-from Employee as e 
-left join Employee as e1 on e.managerId = e1.id
-where e.salary > e1.salary 
-and e.managerId is not null )
-
-select Employee_name as "Employee" from cte
+select Employee from cte
