ecd487b3b4c01dae80cfb2eac314409c0829e71c
Adishjoshi <67081361+Adishjoshi@users.noreply.github.com>
2025-05-11 15:59:26 -0400

    Time: 269 ms (81.13%), Space: 0B (100%) - LeetHub

---
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -0,0 +1,10 @@
+with cte as (
+  select
+    salary
+  , dense_rank() over (order by salary desc) as rk
+  from Employee
+)
+
+select
+  max(case when rk = 2 then salary else null end) as SecondHighestSalary
+from cte;
