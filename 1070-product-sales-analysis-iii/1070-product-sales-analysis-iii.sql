3704b2faf115dca869b9d8a24f59f9dae1ac4ddf
Adishjoshi <67081361+Adishjoshi@users.noreply.github.com>
2025-05-12 22:11:57 -0400

    Time: 1204 ms (32.92%), Space: 0B (100%) - LeetHub

--- 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -1,10 +1,17 @@
-
-
 # Write your MySQL query statement below
+with cte as(
 
-with cte as (
-select product_id, year, quantity, price, rank() over(partition by product_id order by year asc) as rnk
-from Sales)
+    select s.sale_id
+    ,s.product_id
+    ,s.year as first_year
+    ,s.quantity
+    ,s.price
+    
+    ,p.product_name
+    ,rank()over(partition by s.product_id order by s.year) as rk
+    from Sales as s
+    left join Product as p ON s.product_id = p.product_id
+)
 
-select product_id, year as first_year, quantity, price from cte
-where rnk = '1'
+select product_id, first_year, quantity, price from cte 
+where rk = 1
