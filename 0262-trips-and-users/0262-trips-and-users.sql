bf253f10c135707ad7fa8504c842053eaef4b25e
Adishjoshi <67081361+Adishjoshi@users.noreply.github.com>
2025-07-08 21:39:53 -0400

    Time: 525 ms (74.79%), Space: 0B (100%) - LeetHub

--- 0262-trips-and-users/0262-trips-and-users.sql
+++ 0262-trips-and-users/0262-trips-and-users.sql
@@ -1,11 +1,23 @@
-SELECT t.request_at as 'Day',
-    ROUND(
-        (SUM(CASE WHEN t.status LIKE '%cancelled%' THEN 1 ELSE 0 END)) / COUNT(*), 
-        2
-    ) AS 'Cancellation Rate'
-FROM Trips t
-LEFT JOIN Users u1 ON t.client_id = u1.users_id
-LEFT JOIN Users u2 ON t.driver_id = u2.users_id
-WHERE u1.banned = 'No' AND u2.banned = 'No'
-AND t.request_at between '2013-10-01' and '2013-10-03'
-group by t.request_at
+# Write your MySQL query statement below
+with cte as (select 
+t.client_id 
+,t.driver_id
+,t.city_id
+,t.status
+,t.request_at
+,u.users_id
+,u.banned
+,u.role
+from Trips as t 
+left join Users as u on t.client_id = u.users_id
+left join Users as u1 on t.driver_id = u1.users_id 
+where u.banned = 'No' AND u1.banned = 'No' AND t.request_at between '2013-10-01' and '2013-10-03'
+)
+
+select 
+request_at as "Day"
+,ROUND(SUM(CASE when status like '%cancelled%' then 1 else 0 end)/ count(users_id),2) as "Cancellation Rate"
+from cte 
+group by request_at
+
+
