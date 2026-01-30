50701fb16dd25f62e8623c373be7ab484b89a510
Adishjoshi <67081361+Adishjoshi@users.noreply.github.com>
2026-01-30 17:53:30 -0500

    Time: 464 ms (62.67%), Space: 0B (100%) - LeetHub

--- 1112-highest-grade-for-each-student/1112-highest-grade-for-each-student.sql
+++ 1112-highest-grade-for-each-student/1112-highest-grade-for-each-student.sql
@@ -1,36 +1,15 @@
--- Write your PostgreSQL query statement below
-with cte3 as (
-with cte2 as (
-with cte as (
-
-    select
-    student_id
-    ,course_id
-    ,grade
-    ,rank() over(partition by student_id order by grade desc) as rk
-    from Enrollments
-
+with cte as
+(
+Select
+student_id,
+course_id,
+grade,
+dense_rank() over (partition by student_id order by  grade desc, course_id asc) as rn
+From Enrollments
 )
-
-select 
-student_id
-,course_id
-,grade
+Select 
+student_id,
+course_id,
+grade
 from cte
-where  rk = 1
-order by course_id asc
-)
-
-select 
-student_id
-,course_id
-,grade
-,row_number() over(partition by student_id order by course_id) as rnn
-from cte2
-)
-
-select 
-student_id
-,course_id
-,grade
-from cte3 where rnn = 1
+where rn=1
