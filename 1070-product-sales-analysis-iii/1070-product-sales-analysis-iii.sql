1b32e648a8d4378c47880e21b73ffd8b9139ec4d
Adishjoshi <67081361+Adishjoshi@users.noreply.github.com>
2026-02-12 18:51:58 -0500

    Time: 830 ms (87.28%), Space: 0B (100%) - LeetHub

--- 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -1,17 +1,19 @@
-# Write your MySQL query statement below
-with cte as(
+-- Write your PostgreSQL query statement below
+with pfs
+as(
 
-    select s.sale_id
-    ,s.product_id
-    ,s.year as first_year
-    ,s.quantity
-    ,s.price
-    
-    ,p.product_name
-    ,rank()over(partition by s.product_id order by s.year) as rk
-    from Sales as s
-    left join Product as p ON s.product_id = p.product_id
-)
+    select
+    rank() over(partition by product_id order by year asc) as rnn
+    ,product_id
+    ,year as first_year
+    ,quantity
+    ,price
+    from Sales)
 
-select product_id, first_year, quantity, price from cte 
-where rk = 1
+    select 
+    product_id
+    ,first_year
+    ,quantity
+    ,price
+    from pfs
+    where rnn = 1
