51ce6c3602f1d9678ea65f3dab66305ba15a9177
Adishjoshi <67081361+Adishjoshi@users.noreply.github.com>
2026-02-22 22:46:34 -0500

    Time: 484 ms (67.41%), Space: 0B (100%) - LeetHub

--- 1393-capital-gainloss/1393-capital-gainloss.sql
+++ 1393-capital-gainloss/1393-capital-gainloss.sql
@@ -1,12 +1,11 @@
 # Write your MySQL query statement below
 
-with cte as (select * 
-,case 
-when operation = 'Buy' then price * (-1)
-when operation = 'Sell' then price * 1
-end as amt
-from Stocks)
 
-select stock_name, sum(amt) as capital_gain_loss
-from cte
+select
+stock_name
+,sum(case 
+when operation = "Buy" then price * (-1)
+when operation = "Sell" then price 
+End) as capital_gain_loss
+from Stocks
 group by stock_name
