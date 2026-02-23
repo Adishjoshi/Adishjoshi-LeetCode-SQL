2e91ba73f2d05ec7c1939a4e908ee01b5fc1d269
Adishjoshi <67081361+Adishjoshi@users.noreply.github.com>
2026-02-22 19:00:17 -0500

    Time: 243 ms (50.47%), Space: 0B (100%) - LeetHub

--- 1270-all-people-report-to-the-given-manager/1270-all-people-report-to-the-given-manager.sql
+++ 1270-all-people-report-to-the-given-manager/1270-all-people-report-to-the-given-manager.sql
@@ -1,9 +1,24 @@
 # Write your MySQL query statement below
-with cte as (select e.employee_id, e3.manager_id
- from Employees as e 
-left join Employees as e2 on e.manager_id = e2.employee_id
-left join Employees as e3 on e2.manager_id = e3.employee_id)
+with cte as(
+select 
+e.employee_id
+,e.employee_name
+,m.employee_id as mgr_id
+,m.employee_name as mgr_name
+,d.employee_id as dir_id 
+,d.employee_name as dir_name
+,v.employee_id as vp_id 
+,v.employee_name as vp_name
 
-select employee_id
-from cte
-where manager_id = 1 and employee_id <> 1
+from Employees as e 
+left join Employees as m on e.manager_id = m.employee_id 
+left join Employees as d on m.manager_id = d.employee_id
+left join Employees as v on d.manager_id = v.employee_id
+)
+
+select 
+employee_id 
+from cte 
+where vp_id = 1
+and employee_id <> 1
+order by employee_id 
